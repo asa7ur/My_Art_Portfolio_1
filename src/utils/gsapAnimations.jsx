92be1bc0510f0f413536240b344e74fdebe61634
Garik Asatryan <asa7ur@gmail.com>
2024-06-05 16:50:10 +0200

    Added sliding effect to the columns

--- src/utils/gsapAnimations.jsx
+++ src/utils/gsapAnimations.jsx
@@ -13,12 +13,49 @@ export const setupGSAPAnimations = (containerRef) => {
         scrub: 1,
         pin: true,
         start: 'top top',
-        end: '+=1000',
+        end: '+=3300',
       },
     })
+
     .to(container, {
       x: () => -(container.scrollWidth - window.innerWidth) + 'px',
       ease: 'none',
       duration: 1,
+    })
+
+    .to('.col-1', {
+      y: -250,
+      ease: 'none',
+      duration: 2,
+      scrollTrigger: {
+        trigger: '.image-gallery',
+        start: 'top center',
+        end: '+=3300',
+        scrub: 3,
+      },
+    })
+
+    .to('.col-2', {
+      y: 250,
+      ease: 'none',
+      duration: 2,
+      scrollTrigger: {
+        trigger: '.image-gallery',
+        start: 'top center',
+        end: '+=3300',
+        scrub: 3,
+      },
+    })
+    
+    .to('.col-3', {
+      y: -250,
+      ease: 'none',
+      duration: 2,
+      scrollTrigger: {
+        trigger: '.image-gallery',
+        start: 'top center',
+        end: '+=3300',
+        scrub: 3,
+      },
     })
 }
