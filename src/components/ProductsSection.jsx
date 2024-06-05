722cca34fb38a72506678ebec934c4c46aea6cad
Garik Asatryan <asa7ur@gmail.com>
2024-06-05 22:59:42 +0200

    Added a navbar to the products section

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,10 +1,23 @@
 import styled from 'styled-components'
+import { FaArrowDown } from 'react-icons/fa'
 
 const ProductsSection = ({ isVisible, onClose }) => (
   <Wrapper className={isVisible ? 'visible' : ''}>
-    <button className='close-button' onClick={onClose}>
-      Close
-    </button>
+    <div className='navbar'>
+      <div className='nav-container'>
+        <div className='search'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search'
+            className='search-input'
+          />
+        </div>
+        <button className='close-button' onClick={onClose}>
+          <FaArrowDown />
+        </button>
+      </div>
+    </div>
     <div className='content'>Products will be shown here...</div>
   </Wrapper>
 )
@@ -29,14 +42,41 @@ const Wrapper = styled.div`
     top: 0;
   }
 
-  .close-button {
+  .navbar {
     position: absolute;
-    top: 20px;
-    right: 20px;
-    padding: 10px;
-    background-color: red;
-    color: white;
-    border: none;
+    top: 0;
+    width: 100%;
+    background-color: black;
+  }
+
+  .nav-container {
+    width: 100%;
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 1.5rem;
+  }
+
+  .search-input {
+    display: block;
+    font-size: 1rem;
+    padding: 0.5rem;
+    color: black;
+  }
+
+  .search-input::placeholder {
+    text-transform: capitalize;
+    color: black;
+    font-size: 1rem;
+  }
+
+  .close-button {
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: 1px solid var(--grey-600);
+    color: var(--grey-600);
+    background-color: white;
     cursor: pointer;
   }
 `
